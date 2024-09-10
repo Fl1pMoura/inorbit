2b540380f16775991e8007d8e4caa28c3ad0f70f
Felipe Moura <felipenascimento.moura41@gmail.com>
2024-09-10 18:49:51 -0300

    feat: create summary method

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -7,8 +7,13 @@ import {
 import { createCompletionRoute } from './routes/create-completion'
 import { createGoalRoute } from './routes/create-goal'
 import { getPendingGoalsRoute } from './routes/get-pending-goals'
+import { getWeekSummaryRoute } from './routes/get-week-summary'
+import { fastifyCors } from '@fastify/cors'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
+app.register(fastifyCors, {
+  origin: '*',
+})
 
 // Add schema validator and serializer
 app.setValidatorCompiler(validatorCompiler)
@@ -17,5 +22,6 @@ app.setSerializerCompiler(serializerCompiler)
 app.register(createCompletionRoute)
 app.register(createGoalRoute)
 app.register(getPendingGoalsRoute)
+app.register(getWeekSummaryRoute)
 
 app.listen({ port: 3333 }).then(() => console.log('Server runnig'))
